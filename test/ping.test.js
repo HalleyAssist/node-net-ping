56da7ca368993d849aa9fcc2f00770d625f1ec6f
Mathew Heard <splitice@users.noreply.github.com>
2020-11-04 13:13:22 +1100

    allow choice of src address

--- test/ping.test.js
+++ test/ping.test.js
@@ -11,5 +11,14 @@ describe("ping", function(){
             session.close()
         }
     })
-    
+    it('advanced 127.0.0.1 from 127.0.0.1', async function(){
+        this.timeout(1000)
+        const session = NetPing.createSession ({timeout: 800, _debug: true});
+        try {
+            const target = "127.0.0.1"
+            await session.pingHost (target, undefined, {src: "127.0.0.1"});
+        } finally {
+            session.close()
+        }
+    })
 })
